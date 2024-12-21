3bff72896a868a4678f1040fb5a4959a2c565129
Abraham "El Pupas" Hernando <ahernandob@iesrafaelalberti.es>
2024-12-21 12:11:45 +0100

    Create ej_3-1-1.kt

---
+++ Listas/ej_3-1-1.kt
@@ -0,0 +1,10 @@
+/*Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.*/
+fun main () {
+    val asignaturas = listOf("Mates", "Física", "Química", "Historia", "Lengua")
+    println("Las asignaturas que tienes este curso son: ")
+    println(asignaturas[0])
+    println(asignaturas[1])
+    println(asignaturas[2])
+    println(asignaturas[3])
+    println(asignaturas[4])
+}
